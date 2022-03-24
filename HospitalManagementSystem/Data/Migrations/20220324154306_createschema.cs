90c2feb4eeacd49b94a04b8a40bd6588adf5a2c5
sahidha <44166281+sahidha@users.noreply.github.com>
2022-03-25 00:15:47 +0800

    Create Project with Login/Register Identity
    run update-database CreateIdentitySchema in Package Manager Console to create the database into system

---
+++ HospitalManagementSystem/Data/Migrations/20220324154306_createschema.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HospitalManagementSystem.Data.Migrations
+{
+    public partial class createschema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
