b47c4d21263340683535cce30ecd70242b9e4423
WongWeiSheng <wws0879Sheng@gmail.com>
2022-03-28 10:05:47 +0800

    Revert "added required login for patients"
    This reverts commit cdcad9d88999ad15ddf883d66927c0b49062b577.

--- HospitalManagementSystem/Controllers/PatientsController.cs
+++ HospitalManagementSystem/Controllers/PatientsController.cs
@@ -20,7 +20,6 @@ namespace HospitalManagementSystem.Controllers
         }
 
         // GET: Patients
-        [Microsoft.AspNetCore.Authorization.Authorize]
          public async Task<IActionResult> Index(string searchstr)
          {
              var Patients = from p in _context.Patient
